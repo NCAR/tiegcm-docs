2aeabc40e99585a2170e74779e3baebba3968c1e
cookieenick <nikhilr@ucar.edu>
2023-12-03 20:21:10 -0700

    Added python_docs_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = '0.1.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['python_docs_theme']
+extensions = ['sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
