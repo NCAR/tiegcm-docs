2afd064419b066fcdf13bf0b60c80cb5b95b1785
cookieenick <nikhilr@ucar.edu>
2023-12-03 20:37:33 -0700

    modified theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,9 +14,7 @@ release = '0.1.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx_rtd_theme'
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
