dca7a325c76df20e188a8b5db92d4a53749e66c3
cookieenick <nikhilr@ucar.edu>
2024-03-26 00:30:59 -0600

    Change in build info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,9 +7,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'tiegcm-docs'
-copyright = '2023, Nikhil Rao'
+copyright = '2024, Nikhil Rao'
 author = 'Nikhil Rao'
-release = '0.1.1'
+release = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
